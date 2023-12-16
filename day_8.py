c3e0a46082316be9f1bf9ce06d0425d502029f28
jossM <jossM@users.noreply.github.com>
2023-12-16 17:05:11 +0100

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -5,7 +5,7 @@ raw_instructions = """""" # fill this with input
 path, raw_edges = raw_instructions.split('\n\n')
 all_edges = {}
 for node, raw_out_nodes in map(lambda line: line.split("="), filter(bool, raw_edges.split("\n"))):
-    all_edges[node] = tuple(map(lambda s: s.strip(), raw_out_nodes.strip(" ()").split(",")))
+    all_edges[node.strip()] = tuple(map(lambda s: s.strip(), raw_out_nodes.strip(" ()").split(",")))
 
 # part 1
 current_location = 'AAA'
@@ -29,8 +29,16 @@ shortest_path = find_smallest_repeating_pattern(path)
 
 starting_locations = [node for node in all_edges.keys() if node.endswith('A')]
 location_pattern_length = []
-node_Z_patterns = []
+all_node_Z_patterns = []
 
 for node in starting_locations:
-    continue
-    # to be continued
+    step_count = 0
+    node_patterns = []
+    while (step_count % len(shortest_path), node) not in  node_patterns:
+        node_patterns.append(step_count % len(shortest_path), node)
+        node = all_edges[node][0 if shortest_path[step_count % len(shortest_path)] == 'L' else 1]
+        step_count += 1
+    node_cycle = find_smallest_repeating_pattern([node for _, node in node_patterns])
+    all_node_Z_patterns.append((len(node_cycle), [step_count for step_count, node in enumerate(node_cycle) if node.endswith('Z')]))
+
+# to be continued
