98d2c5eb4faf452d4f397d0eadd14ef869c5f1e3
jossM <jossM@users.noreply.github.com>
2023-12-17 19:54:42 +0100

    bugged part 2

--- day_10.py
+++ day_10.py
@@ -63,54 +63,70 @@ while all_search_locations:
     all_search_locations = next_all_search_locations
 if part_1:
     print(max([dist for line in distance_from_start for dist in line if dist is not None]))
-else:
-    # to analyse things easily let us imagine an interlocked tiles in between the intersections of each point.
-    # in the figure below + is the original points and x the interlocked ones :
-    # index interlocked 0 : x   x   x
-    # index main 0        :   +   +
-    # index interlocked 1 : x   x   x
-    # index main 1        :   +   +
-    external_interlocked_tiles = (
-        {(0, i) for i in range(len(pipe_map) + 1)}
-        | {(len(pipe_map), i) for i in range(len(pipe_map) + 1)}
-        | {(i, 0) for i in range(len(pipe_map[0]) + 1)}
-        | {(i, len(pipe_map[0])) for i in range(len(pipe_map[0]) + 1)}
-    )
-    previous_additional_tiles = set(external_interlocked_tiles)
-    while previous_additional_tiles:
-        new_external_interlocked_tiles = set()
-        for x, y in previous_additional_tiles:
-            # up left
-            if 0 < x and 0 < y:
-                up_left = main_tiles_connections[x-1][y-1]
-                if not up_left.down:
-                    new_external_interlocked_tiles.add((x, y-1))
-                if not up_left.right:
-                    new_external_interlocked_tiles.add((x-1, y))
-            elif 0 < x:
-                up_right = main_tiles_connections[x-1][y]
-                if not up_right.left:
-                    new_external_interlocked_tiles.add((x-1, y))
-            elif 0 < y:
-                down_left = main_tiles_connections[x][y-1]
-                if not down_left.up:
-                    new_external_interlocked_tiles.add((x, y-1))
-            # down right
-            if x < len(connected_locations) and y < len(connected_locations[x]):
-                down_right = main_tiles_connections[x][y]
-                if not down_right.up:
-                    new_external_interlocked_tiles.add((x, y+1))
-                if not down_right.left:
-                    new_external_interlocked_tiles.add((x+1, y))
-            elif x < len(connected_locations):
-                down_left = main_tiles_connections[x][y-1]
-                if not down_left.right:
-                    new_external_interlocked_tiles.add((x+1, y))
-            elif y < len(connected_locations[0]):
-                up_right = main_tiles_connections[x-1][y]
-                if not up_right.down:
-                    new_external_interlocked_tiles.add((x, y+1))
-        new_external_interlocked_tiles -= previous_additional_tiles
-        print(new_external_interlocked_tiles)
-  
-# WIP
+
+# to analyse things easily let us imagine an interlocked tiles in between the intersections of each point.
+# in the figure below + is the original points and x the interlocked ones :
+# index interlocked 0 : x   x   x
+# index main 0        :   +   +
+# index interlocked 1 : x   x   x
+# index main 1        :   +   +
+external_interlocked_tiles = (
+    {(0, i) for i in range(len(pipe_map) + 1)}
+    | {(len(pipe_map), i) for i in range(len(pipe_map) + 1)}
+    | {(i, 0) for i in range(len(pipe_map[0]) + 1)}
+    | {(i, len(pipe_map[0])) for i in range(len(pipe_map[0]) + 1)}
+)
+previous_additional_tiles = set(external_interlocked_tiles)
+while previous_additional_tiles:
+    new_external_interlocked_tiles = set()
+    for x, y in previous_additional_tiles:
+        # up left
+        if 0 < x and 0 < y:
+            up_left = main_tiles_connections[x-1][y-1]
+            if not up_left.down:
+                new_external_interlocked_tiles.add((x, y-1))
+            if not up_left.right:
+                new_external_interlocked_tiles.add((x-1, y))
+        elif 0 < x:
+            up_right = main_tiles_connections[x-1][y]
+            if not up_right.left:
+                new_external_interlocked_tiles.add((x-1, y))
+        elif 0 < y:
+            down_left = main_tiles_connections[x][y-1]
+            if not down_left.up:
+                new_external_interlocked_tiles.add((x, y-1))
+        # down right
+        if x < len(connected_locations) and y < len(connected_locations[x]):
+            down_right = main_tiles_connections[x][y]
+            if not down_right.up:
+                new_external_interlocked_tiles.add((x, y+1))
+            if not down_right.left:
+                new_external_interlocked_tiles.add((x+1, y))
+        elif x < len(connected_locations):
+            down_left = main_tiles_connections[x][y-1]
+            if not down_left.right:
+                new_external_interlocked_tiles.add((x+1, y))
+        elif y < len(connected_locations[0]):
+            up_right = main_tiles_connections[x-1][y]
+            if not up_right.down:
+                new_external_interlocked_tiles.add((x, y+1))
+    new_external_interlocked_tiles -= external_interlocked_tiles
+    previous_additional_tiles = new_external_interlocked_tiles - external_interlocked_tiles
+    external_interlocked_tiles |= new_external_interlocked_tiles
+
+main_tiles_belonging = []
+for x, line in enumerate(pipe_map):
+    belonging_line = ""
+    for y, char in enumerate(line):
+        local_external_interlocked_tiles = external_interlocked_tiles.intersection([(x, y), (x+1, y), (x, y+1), (x+1, y+1)])
+        if len(local_external_interlocked_tiles) == 0:
+            belonging_line += "*"
+        elif len(local_external_interlocked_tiles) == 4:
+            belonging_line += "O"
+        else:
+            belonging_line += char
+            
+    main_tiles_belonging.append(belonging_line)
+    
+print("\n".join(main_tiles_belonging))
+    
