c1188f3a5e35dc9aeac3ff8fba2cc798e45ce2d4
jossM <jossM@users.noreply.github.com>
2023-12-18 22:01:09 +0100

    Update day_11.py

--- day_11.py
+++ day_11.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 
 raw_galaxy_image = list(filter(bool, """""".split("\n"))) # fill this with input
-
+part_1 = True
 
 galaxies_index = [(row_index, column_index) for row_index, line in enumerate(raw_galaxy_image) for column_index, char in enumerate(line) if char == "#"]
 if not galaxies_index:
@@ -17,6 +17,10 @@ empty_cols = {col_index for col_index in range(galaxy_image.shape[1]) if not gal
 
 galaxy_distances = np.full((len(galaxies_index), len(galaxies_index)), np.NaN)
 
+if part_1:
+    expansion = 2 - 1
+else:
+    expansion = 1000000 - 1
 for g_index_1, (x1, y1) in zip(range(len(galaxies_index) - 1), galaxies_index[:-1]):
     for g_index_2, (x2, y2) in zip(range(g_index_1 + 1, len(galaxies_index)), galaxies_index[g_index_1 + 1:]):
         galaxy_distances[g_index_1, g_index_2] = int(sum([
@@ -26,4 +30,4 @@ for g_index_1, (x1, y1) in zip(range(len(galaxies_index) - 1), galaxies_index[:-
                 len(empty_cols.intersection(range(min(y1, y2), max(y1, y2)))),  # space distorsion in y
         ]))
 
-np.nansum(galaxy_distances)
+print(f"distance {int(np.nansum(galaxy_distances))})
