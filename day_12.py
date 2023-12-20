f8fe53c54e5d263beff33a1acdd3adda601b32d8
jossM <jossM@users.noreply.github.com>
2023-12-20 10:31:07 +0100

    Update day_12.py

--- day_12.py
+++ day_12.py
@@ -3,14 +3,16 @@ from typing import List, Tuple, Iterable
 raw_spring_patterns = list(filter(bool, """""".split("\n"))) # fill this with input
 
 
+
 def describe_pattern(springs_pattern: str) -> List[Tuple[str, int]]:
     group_description = []
     while springs_pattern:
         start_char = springs_pattern[0]
-        start_group = next(re.finditer(springs_pattern[0]) + "+", springs_pattern))
+        start_group = next(re.finditer(springs_pattern[0]) + "+", springs_pattern)
         group_description.append((start_char, start_group.end()))
         springs_pattern = springs_pattern[start_group.end():]
     return group_description
+
 
 def start_pattern_could_match(springs_pattern: List[Tuple[str, int]], broken_springs_spec: List[int]) -> bool:
     next_group_index = None
@@ -23,20 +25,21 @@ def start_pattern_could_match(springs_pattern: List[Tuple[str, int]], broken_spr
         if char == "?":
             has_unknown_slots = True
             break
-  
+
     if not has_unknown_slots:
         return broken_spring_pattern == broken_springs_spec
-    
+
     exact_match_length = len(broken_spring_pattern)
     if springs_pattern[next_group_index][0] == "?":
         exact_match_length -= 1
-    
+
     return all([
         broken_spring_pattern[:exact_match_length] == broken_springs_spec[:exact_match_length],
         broken_spring_pattern[exact_match_length - 1] <= broken_springs_spec[exact_match_length - 1]
-    )
+    ])
 
-def fuse_same_adjacent_groups(spring_pattern: List[Tuple[str, int]]) -> List[Tuple[str, int]:
+
+def fuse_same_adjacent_groups(spring_pattern: List[Tuple[str, int]]) -> List[Tuple[str, int]]:
     result = []
     previous_group_char = None
     for group_char, group_size in spring_pattern:
@@ -48,23 +51,24 @@ def fuse_same_adjacent_groups(spring_pattern: List[Tuple[str, int]]) -> List[Tup
     return result
 
 
-def find_patterns(spring_pattern: List[Tuple[str, int]], broken_springs_spec: List[int]) -> Iterable[str]
+def find_patterns(spring_pattern: List[Tuple[str, int]], broken_springs_spec: List[int]) -> Iterable[str]:
     if not start_pattern_could_match(spring_pattern, broken_springs_spec):
         return
     if not any("?" == char for char, _ in raw_spring_pattern):
         yield ''.join(char * group_size for char, group_size in raw_spring_pattern)
         return
-    first_question_group_index, question_group_size = next((group_index, group_size) for group_index, (group_char, group_size) in enumerate(spring_pattern) if group_char == "?" )
+    first_question_group_index, question_group_size = next(
+        (group_index, group_size) for group_index, (group_char, group_size) in enumerate(spring_pattern) if group_char == "?")
     if question_group_size > 1:
-        remainging_question_group = [('?', question_group_size-1)]
+        remainging_question_group = [('?', question_group_size - 1)]
     else:
         remainging_question_group = []
-    
-    for group_choice in [("#",1), (".",1)]:
+
+    for group_choice in [("#", 1), (".", 1)]:
         yield from find_patterns(fuse_same_adjacent_groups(
-            spring_pattern[:first_question_group_index] 
+            spring_pattern[:first_question_group_index]
             + [group_choice]
             + remainging_question_group
-            + spring_pattern[first_question_group_index+1:]
+            + spring_pattern[first_question_group_index + 1:]
         ))
-
+        
